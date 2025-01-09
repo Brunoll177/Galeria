738f9f74acceaca9090525a43631ebd6cccbd568
Brunoll177 <brunolopesdev19@gmail.com>
2025-01-09 19:43:23 -0300

    att

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 const search = document.querySelector(".search")
 const names = document.querySelectorAll(".image")
+const noResults = document.querySelector(".nada")
 search.addEventListener("input", function(e){
+    let hasResults = false;
     const value = e.target.value
     names.forEach(name => {
         if(format(name.textContent).indexOf(format(value)) !== -1 ){
             name.style.display = "block"
+             hasResults = true;
         }else{
             name.style.display = "none"
         }
     })
+    console.log(hasResults)
+    if(hasResults){
+         noResults.style.display = "none"
+    }else{
+         noResults.style.display = "block"
+    }
 })
 function format(v){
     return v.toLowerCase().trim()
